376eeff12f571b8982b4e9a9ba3ba15c454e585a
WillWhittakerDHP <will@districthomepro.com>
2024-09-11 19:40:02 -0400

    first pseudocode attempt and fill out the assets branch

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4, 5];
+
+function sum(numbers) {
+  if (length === 0) return 0;
+  let total = 0;  
+  for (let i = 0; i < length; i++) {
+    (total += numbers[i].value);  
+    return total;
+  };
+};
+
+sum();
