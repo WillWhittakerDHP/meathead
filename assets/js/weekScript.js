042f21ef9629709c066a35f36ec02f8e011549ba
Marcel Peyton <marcel-p@outlook.com>
2024-09-17 19:12:13 -0400

    Not Finished

---
+++ assets/js/weekScript.js
@@ -0,0 +1,11 @@
+function cardColumnCreator(numberOfColumns, focusSets,accessorySets){
+  for (let i = 1; i < numberOfColumns; i++) {    
+    var first_col = document.createElement('div.col');
+    var card = document.createElement('div.card');
+    var cardBody = document.createElement('div.card-body');
+    var outerContainer = document.createElement('div');
+    outerContainer.classList.add('');
+  }
+}
+
+cardColumnCreator(2,0)
